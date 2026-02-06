c0898bd89f05def8981813c526727b36d2cf10e1
JEAN_BAPTISTE MATTEI <JEAN_BAPTISTE_MATTEI@gmail.com>
2026-02-06 23:29:40 +0100

    je parle pas fr

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -102,8 +102,7 @@ fn handle_admin_login(app: &mut BindKeyApp) {
         app.login_status = "Champs invalides".to_string();
         return;
     }
-    app.role_user = Role::ADMIN;
-    app.current_page = Page::Home;
+
     app.login_status = "Authentification Admin en cours...".to_string();
 
     let clone_sender = app.sender.clone();
